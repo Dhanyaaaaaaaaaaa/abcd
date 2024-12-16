49e2ea73bc0151ca40ee0ee40f8175f5276a11f7
Sumadhva <Sumadhva.V@visionetsystems.com>
2024-12-16 13:34:30 +0530

    Add generated files for orders

---
+++ orders/src/main/java/org/openapitools/model/OrdersGet200ResponseInner.java
@@ -0,0 +1,134 @@
+package org.openapitools.model;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import java.math.BigDecimal;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import javax.annotation.Generated;
+
+/**
+ * OrdersGet200ResponseInner
+ */
+
+@JsonTypeName("_orders_get_200_response_inner")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-12-16T13:34:13.909231500+05:30[Asia/Calcutta]", comments = "Generator version: 7.9.0")
+public class OrdersGet200ResponseInner {
+
+  private String id;
+
+  private BigDecimal amount;
+
+  private String status;
+
+  public OrdersGet200ResponseInner id(String id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Get id
+   * @return id
+   */
+  
+  @Schema(name = "id", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("id")
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public OrdersGet200ResponseInner amount(BigDecimal amount) {
+    this.amount = amount;
+    return this;
+  }
+
+  /**
+   * Get amount
+   * @return amount
+   */
+  @Valid 
+  @Schema(name = "amount", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("amount")
+  public BigDecimal getAmount() {
+    return amount;
+  }
+
+  public void setAmount(BigDecimal amount) {
+    this.amount = amount;
+  }
+
+  public OrdersGet200ResponseInner status(String status) {
+    this.status = status;
+    return this;
+  }
+
+  /**
+   * Get status
+   * @return status
+   */
+  
+  @Schema(name = "status", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("status")
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    OrdersGet200ResponseInner ordersGet200ResponseInner = (OrdersGet200ResponseInner) o;
+    return Objects.equals(this.id, ordersGet200ResponseInner.id) &&
+        Objects.equals(this.amount, ordersGet200ResponseInner.amount) &&
+        Objects.equals(this.status, ordersGet200ResponseInner.status);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, amount, status);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class OrdersGet200ResponseInner {\n");
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    amount: ").append(toIndentedString(amount)).append("\n");
+    sb.append("    status: ").append(toIndentedString(status)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
